a1bedcb4004f438978319f5e19fd3120f30a17c9
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2025-06-19 11:12:34 +0530

    added the two binding

--- src/app/menu-items/menu-items.ts
+++ src/app/menu-items/menu-items.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormControlName,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-menu-items',
-  imports: [MatInputModule, MatFormFieldModule, FormsModule],
+  imports: [
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './menu-items.html',
   styleUrl: './menu-items.scss',
 })
 export class MenuItems {
-  userName: string = '';
+  newUserName: any;
+  textFormGroup = new FormGroup({
+    newUserName: new FormControl(''),
+  });
 }
