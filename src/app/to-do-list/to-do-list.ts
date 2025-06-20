33203e357df97c1bcc5be5318297004d2eadf97d
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2025-06-20 15:41:27 +0530

    pipes and directive to used our component

--- src/app/to-do-list/to-do-list.ts
+++ src/app/to-do-list/to-do-list.ts
@@ -1,14 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { LastWordCapPipe } from './last-word.cap.pipe';
+import { ToDoListColorDirective } from './to-do-list-color.directive';
 import { IToDoList } from './to-do-list.model';
 import { ToDoListService } from './to-do-list.service';
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-to-do-list',
-  imports: [MatTableModule, MatIconModule, MatCardModule, HttpClientModule],
+  imports: [
+    MatTableModule,
+    MatIconModule,
+    MatCardModule,
+    HttpClientModule,
+    ToDoListColorDirective,
+    LastWordCapPipe,
+  ],
   providers: [ToDoListService],
   templateUrl: './to-do-list.html',
   styleUrl: './to-do-list.scss',
